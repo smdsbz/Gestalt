4c6c81096bf0f5583e852c4227de4a541070513e
smdsbz <694066681@qq.com>
2021-12-15 17:49:07 +0800

    fix dependencies so i can compile this on aep server

--- install-deps-ub21.sh
+++ install-deps-ub21.sh
@@ -3,7 +3,7 @@
 set -x
 
 sudo apt update
-sudo apt install -y cmake ninja-build
+sudo apt install -y cmake ninja-build g++-11
 sudo apt install -y libibverbs-dev librdmacm-dev
 sudo apt install -y libpmem-dev librpmem-dev libpmemblk-dev libpmemlog-dev libpmemobj-dev libpmempool-dev libpmempool-dev
 sudo apt install -y libboost-all-dev
